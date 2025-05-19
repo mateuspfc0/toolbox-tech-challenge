cd9771507190ce62474ae6fbcd1eed3d5fa96dff
Mateus Ribeiro <64538183+mateuspfc0@users.noreply.github.com>
2025-05-19 08:01:05 -0300

    Update logging level

--- api/server.js
+++ api/server.js
@@ -2,5 +2,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}`);
+  console.debug(`API server running on port ${PORT}`);
 });
